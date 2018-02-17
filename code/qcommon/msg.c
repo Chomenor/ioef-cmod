ffaeaedfb2916df7deacac894b583d5b6a700848
Noah Metzger <chomenor@gmail.com>
2018-02-17 17:44:51 -0600

    cmod: Restore delta message functions removed in ioq3

--- code/qcommon/msg.c
+++ code/qcommon/msg.c
@@ -682,6 +682,24 @@ extern cvar_t *cl_shownet;
 
 #define	LOG(x) if( cl_shownet && cl_shownet->integer == 4 ) { Com_Printf("%s ", x ); };
 
+#ifdef ELITEFORCE
+void MSG_WriteDelta( msg_t *msg, int oldV, int newV, int bits ) {
+	if ( oldV == newV ) {
+		MSG_WriteBits( msg, 0, 1 );
+		return;
+	}
+	MSG_WriteBits( msg, 1, 1 );
+	MSG_WriteBits( msg, newV, bits );
+}
+
+int	MSG_ReadDelta( msg_t *msg, int oldV, int bits ) {
+	if ( MSG_ReadBits( msg, 1 ) ) {
+		return MSG_ReadBits( msg, bits );
+	}
+	return oldV;
+}
+#endif
+
 /*
 =============================================================================
 
