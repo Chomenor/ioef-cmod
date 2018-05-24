e40501f79c62b22021ca531d07e3e1f606b5cd52
Noah Metzger <chomenor@gmail.com>
2018-05-24 00:18:02 -0500

    new filesystem: Print pk3 hash and system pak position in path command

--- code/filesystem/fs_commands.c
+++ code/filesystem/fs_commands.c
@@ -184,6 +184,7 @@ static void FS_Path_f( void ) {
 					char buffer[FS_FILE_BUFFER_SIZE];
 					fs_file_to_buffer((fsc_file_t *)pak, buffer, sizeof(buffer), qfalse, qtrue, qfalse, qfalse);
 					Com_Printf("%s (%i files)\n", buffer, pak->pk3_subfile_count);
+					Com_Printf("    hash(%i) system_pk3_position(%i)\n", (int)pak->pk3_hash, system_pk3_position(pak->pk3_hash));
 					if(fs_connected_server_pure_state()) Com_Printf("    %son the pure list\n",
 							pk3_list_lookup(&connected_server_pk3_list, pak->pk3_hash, qfalse) ? "" : "not "); } } } }
 
