Chomenor/ioef-cmod

commit a165cddbc462dc2c78fc7b40d4051097112ebd3c

new filesystem: Make filesystem code more C++ friendly

code/filesystem/fscore/fsc_misc.c
@@ -115,7 +115,7 @@ static void stack_add_bucket(fsc_stack_t *stack) {
 		new_size = stack->buckets_size * 2;
 		if(new_size > STACK_MAX_BUCKETS) new_size = STACK_MAX_BUCKETS;
 
-		new_allocation = fsc_malloc(new_size * sizeof(fsc_stack_bucket_t *));
+		new_allocation = (fsc_stack_bucket_t **)fsc_malloc(new_size * sizeof(fsc_stack_bucket_t *));
 		fsc_memcpy(new_allocation, stack->buckets, (stack->buckets_position+1)*sizeof(fsc_stack_bucket_t *));
 		fsc_free(stack->buckets);
 		stack->buckets = new_allocation;
@@ -123,13 +123,13 @@ static void stack_add_bucket(fsc_stack_t *stack) {
 
 	// Stack allocations are assumed to be zeroed
 	// so it needs to be accounted for if you change the allocation method here
-	stack->buckets[stack->buckets_position] = fsc_calloc(STACK_BUCKET_SIZE);
+	stack->buckets[stack->buckets_position] = (fsc_stack_bucket_t *)fsc_calloc(STACK_BUCKET_SIZE);
 	stack->buckets[stack->buckets_position]->position = 0; }
 
 void fsc_stack_initialize(fsc_stack_t *stack) {
 	stack->buckets_position = -1;	// stack_add_bucket will increment to 0
 	stack->buckets_size = STACK_INITIAL_BUCKETS;
-	stack->buckets = fsc_malloc(stack->buckets_size * sizeof(fsc_stack_bucket_t *));
+	stack->buckets = (fsc_stack_bucket_t **)fsc_malloc(stack->buckets_size * sizeof(fsc_stack_bucket_t *));
 	stack_add_bucket(stack); }
 
 int fsc_stack_validate(const fsc_stack_t *stack, const fsc_stackptr_t pointer) {
@@ -171,7 +171,7 @@ fsc_stackptr_t fsc_stack_allocate(fsc_stack_t *stack, unsigned int size) {
 		aligned_position = (bucket->position + 3) & ~3; }
 
 	// Allocate the entry
-	output = &bucket->data[aligned_position];
+	output = (char *)bucket + sizeof(*bucket) + aligned_position;
 	bucket->position = aligned_position + size;
 
 	return stack->buckets_position * STACK_BUCKET_SIZE + (unsigned int)(output - (char *)bucket); }
@@ -205,7 +205,8 @@ int fsc_stack_export(fsc_stack_t *stack, fsc_stream_t *stream) {
 	// Write each bucket (current position followed by data)
 	for(i=0; i<=stack->buckets_position; ++i) {
 		if(fsc_write_stream_data(stream, &stack->buckets[i]->position, 4)) return 1;
-		if(fsc_write_stream_data(stream, &stack->buckets[i]->data, stack->buckets[i]->position)) return 1; }
+		if(fsc_write_stream_data(stream, (char *)stack->buckets[i] + sizeof(stack->buckets[i]),
+				stack->buckets[i]->position)) return 1; }
 
 	return 0; }
 
@@ -220,14 +221,15 @@ int fsc_stack_import(fsc_stack_t *stack, fsc_stream_t *stream) {
 	// Allocate bucket array
 	stack->buckets_size = stack->buckets_position + 1;
 	if(stack->buckets_size < STACK_INITIAL_BUCKETS) stack->buckets_size = STACK_INITIAL_BUCKETS;
-	stack->buckets = fsc_calloc(stack->buckets_size * sizeof(fsc_stack_bucket_t *));
+	stack->buckets = (fsc_stack_bucket_t **)fsc_calloc(stack->buckets_size * sizeof(fsc_stack_bucket_t *));
 
 	// Read data for each bucket
 	for(i=0; i<=stack->buckets_position; ++i) {
-		stack->buckets[i] = fsc_calloc(STACK_BUCKET_SIZE);
+		stack->buckets[i] = (fsc_stack_bucket_t *)fsc_calloc(STACK_BUCKET_SIZE);
 		if(fsc_read_stream_data(stream, &stack->buckets[i]->position, 4)) goto error;
 		if(stack->buckets[i]->position >= STACK_BUCKET_SIZE) goto error;
-		if(fsc_read_stream_data(stream, &stack->buckets[i]->data, stack->buckets[i]->position)) goto error; }
+		if(fsc_read_stream_data(stream, (char *)stack->buckets[i] + sizeof(stack->buckets[i]),
+				stack->buckets[i]->position)) goto error; }
 	return 0;
 
 	error:
@@ -242,7 +244,7 @@ void fsc_hashtable_initialize(fsc_hashtable_t *ht, fsc_stack_t *stack, int bucke
 	if(bucket_count < 1) bucket_count = 1;
 	if(bucket_count > FSC_HASHTABLE_MAX_BUCKETS) bucket_count = FSC_HASHTABLE_MAX_BUCKETS;
 	ht->bucket_count = bucket_count;
-	ht->buckets = fsc_calloc(sizeof(fsc_stackptr_t) * bucket_count);
+	ht->buckets = (fsc_stackptr_t *)fsc_calloc(sizeof(fsc_stackptr_t) * bucket_count);
 	ht->utilization = 0;
 	ht->stack = stack; }
 
@@ -257,7 +259,7 @@ fsc_stackptr_t fsc_hashtable_next(fsc_hashtable_iterator_t *iterator) {
 
 void fsc_hashtable_insert(fsc_stackptr_t entry_ptr, unsigned int hash, fsc_hashtable_t *ht) {
 	// entry_ptr must be castable to fsc_hashtable_entry_t.
-	fsc_hashtable_entry_t *entry = fsc_stack_retrieve(ht->stack, entry_ptr);
+	fsc_hashtable_entry_t *entry = (fsc_hashtable_entry_t *)fsc_stack_retrieve(ht->stack, entry_ptr);
 	fsc_stackptr_t *bucket = &ht->buckets[hash % ht->bucket_count];
 	entry->next = *bucket;
 	*bucket = entry_ptr;
@@ -283,7 +285,7 @@ int fsc_hashtable_import(fsc_hashtable_t *ht, fsc_stack_t *stack, fsc_stream_t *
 	if(fsc_read_stream_data(stream, &ht->bucket_count, 4)) return 1;
 	if(ht->bucket_count < 1 || ht->bucket_count > FSC_HASHTABLE_MAX_BUCKETS) return 1;
 	if(fsc_read_stream_data(stream, &ht->utilization, 4)) return 1;
-	ht->buckets = fsc_malloc(ht->bucket_count * sizeof(*ht->buckets));
+	ht->buckets = (fsc_stackptr_t *)fsc_malloc(ht->bucket_count * sizeof(*ht->buckets));
 	if(fsc_read_stream_data(stream, ht->buckets, ht->bucket_count * sizeof(*ht->buckets))) {
 		fsc_hashtable_free(ht);
 		return 1; }
@@ -305,15 +307,15 @@ fsc_stackptr_t fsc_string_repository_getentry(const char *input, int allocate, f
 	// Get entry
 	fsc_hashtable_open(string_repository, hash, &hti);
 	while((sre_ptr = fsc_hashtable_next(&hti))) {
-		sre = STACKPTRL(sre_ptr);
-		if(!fsc_strcmp(sre->string, input)) break; }
+		sre = (stringrepository_entry_t *)STACKPTRL(sre_ptr);
+		if(!fsc_strcmp((char *)sre + sizeof(*sre), input)) break; }
 
 	if(!sre_ptr && allocate) {
 		// Allocate new entry
 		int length = fsc_strlen(input) + 1;		// Include null terminator
 		sre_ptr = fsc_stack_allocate(stack, sizeof(stringrepository_entry_t) + length);
-		sre = STACKPTRL(sre_ptr);
-		fsc_memcpy(sre->string, input, length);
+		sre = (stringrepository_entry_t *)STACKPTRL(sre_ptr);
+		fsc_memcpy((char *)sre + sizeof(*sre), input, length);
 		fsc_hashtable_insert(sre_ptr, hash, string_repository); }
 
 	return sre_ptr; }
@@ -353,7 +355,7 @@ const char *fsc_get_qpath_conversion_table(void) {
 	qpath_conversion_table_initialized = 1;
 	return qpath_conversion_table; }
 
-int fsc_process_qpath(const char *input, char *buffer, char **qp_dir, char **qp_name, char **qp_ext) {
+int fsc_process_qpath(const char *input, char *buffer, const char **qp_dir, const char **qp_name, const char **qp_ext) {
 	// Breaks input path into sanitized directory, name, and extension sections.
 	// Buffer is used to store the separated path data and should be length FSC_MAX_QPATH.
 	// Output qp_dir and qp_ext may be null if no directory or extension is available.
@@ -392,7 +394,7 @@ int fsc_process_qpath(const char *input, char *buffer, char **qp_dir, char **qp_
 
 	return i; }
 
-int fsc_get_leading_directory(const char *input, char *buffer, int buffer_length, char **remainder) {
+int fsc_get_leading_directory(const char *input, char *buffer, int buffer_length, const char **remainder) {
 	// Writes leading directory (text before first slash) to buffer
 	// Writes pointer to remaining (post-slash) string to remainder, or null if not found
 	// Returns number of chars written to output (NOT including null terminator)
@@ -419,12 +421,12 @@ int fsc_get_leading_directory(const char *input, char *buffer, int buffer_length
 // Error Handling
 /* ******************************************************************************** */
 
-void fsc_report_error(fsc_errorhandler_t *errorhandler, int id, char *msg, void *current_element) {
+void fsc_report_error(fsc_errorhandler_t *errorhandler, int id, const char *msg, void *current_element) {
 	if(!errorhandler) return;
 	errorhandler->handler(id, msg, current_element, errorhandler->context); }
 
 void fsc_initialize_errorhandler(fsc_errorhandler_t *errorhandler,
-		void (*handler)(int id, char *msg, void *current_element, void *context), void *context) {
+		void (*handler)(int id, const char *msg, void *current_element, void *context), void *context) {
 	errorhandler->handler = handler;
 	errorhandler->context = context; }
 

code/filesystem/fscore/fscore.h
@@ -71,7 +71,7 @@ typedef unsigned int fsc_stackptr_t;
 
 typedef struct {
 	unsigned int position;
-	char data[];
+	// data added after structure
 } fsc_stack_bucket_t;
 
 typedef struct {
@@ -123,8 +123,7 @@ int fsc_hashtable_import(fsc_hashtable_t *ht, fsc_stack_t *stack, fsc_stream_t *
 typedef struct stringrepository_entry_s {
 	// Hash table compliance
 	fsc_hashtable_entry_t hte;
-
-	char string[];
+	// data added after structure
 } stringrepository_entry_t;
 
 fsc_stackptr_t fsc_string_repository_getentry(const char *input, int allocate, fsc_hashtable_t *string_repository, fsc_stack_t *stack);	// stringrepository_entry_t
@@ -133,8 +132,8 @@ fsc_stackptr_t fsc_string_repository_getstring(const char *input, int allocate, 
 // ***** Qpath Handling *****
 
 const char *fsc_get_qpath_conversion_table(void);
-int fsc_process_qpath(const char *input, char *buffer, char **qp_dir, char **qp_name, char **qp_ext);
-int fsc_get_leading_directory(const char *input, char *output, int buffer_length, char **remainder);
+int fsc_process_qpath(const char *input, char *buffer, const char **qp_dir, const char **qp_name, const char **qp_ext);
+int fsc_get_leading_directory(const char *input, char *output, int buffer_length, const char **remainder);
 
 // ***** Error Handling *****
 
@@ -145,13 +144,13 @@ int fsc_get_leading_directory(const char *input, char *output, int buffer_length
 #define FSC_ERROR_CROSSHAIRFILE 4	// current_element: fsc_file_t
 
 typedef struct {
-	void (*handler)(int id, char *msg, void *current_element, void *context);
+	void (*handler)(int id, const char *msg, void *current_element, void *context);
 	void *context;
 } fsc_errorhandler_t;
 
 void fsc_initialize_errorhandler(fsc_errorhandler_t *errorhandler,
-		void (*handler)(int id, char *msg, void *current_element, void *context), void *context);
-void fsc_report_error(fsc_errorhandler_t *errorhandler, int id, char *msg, void *current_element);
+		void (*handler)(int id, const char *msg, void *current_element, void *context), void *context);
+void fsc_report_error(fsc_errorhandler_t *errorhandler, int id, const char *msg, void *current_element);
 
 /* ******************************************************************************** */
 // Game Parsing Support (fsc_gameparse.c)
@@ -208,7 +207,7 @@ void fsc_memcpy(void *dst, const void *src, unsigned int size);
 int fsc_memcmp(const void *str1, const void *str2, unsigned int size);
 void fsc_memset(void *dst, int value, unsigned int size);
 void fsc_strncpy(char *dst, const char *src, unsigned int size);
-void fsc_strncpy_lower(char *dst, char *src, unsigned int size);
+void fsc_strncpy_lower(char *dst, const char *src, unsigned int size);
 int fsc_strcmp(const char *str1, const char *str2);
 int fsc_stricmp(const char *str1, const char *str2);
 int fsc_strlen(const char *str);

code/client/snd_codec.c
@@ -39,7 +39,7 @@ static void *S_CodecGetSound(const char *filename, snd_info_t *info)
 #ifdef NEW_FILESYSTEM
 	char localName[MAX_QPATH];
 	const fsc_file_t *file;
-	char *extension;
+	const char *extension;
 	snd_codec_t *codec;
 
 	COM_StripExtension(filename, localName, MAX_QPATH);

code/filesystem/fs_commands.c
@@ -75,8 +75,8 @@ static void cmd_indexcache_write(void) {
 	fs_indexcache_write(); }
 
 static void FS_Dir_f( void ) {
-	char	*path;
-	char	*extension;
+	const char	*path;
+	const char	*extension;
 	char	**dirnames;
 	int		ndirs;
 	int		i;
@@ -106,7 +106,7 @@ static void FS_Dir_f( void ) {
 }
 
 static void FS_NewDir_f( void ) {
-	char	*filter;
+	const char	*filter;
 	char	**dirnames;
 	int		ndirs;
 	int		i;
@@ -133,7 +133,7 @@ static void FS_NewDir_f( void ) {
 static void FS_Which_f( void ) {
 	// The lookup functions are more powerful, but this is kept for
 	// users who are familiar with it
-	char *filename = Cmd_Argv(1);
+	const char *filename = Cmd_Argv(1);
 	const fsc_file_t *file;
 
 	if ( !filename[0] ) {
@@ -178,8 +178,8 @@ static void FS_Path_f( void ) {
 
 		for(i=0; i<fs.pk3_hash_lookup.bucket_count; ++i) {
 			fsc_hashtable_open(&fs.pk3_hash_lookup, i, &hti);
-			while((hash_entry = STACKPTR(fsc_hashtable_next(&hti)))) {
-				const fsc_file_direct_t *pak = STACKPTR(hash_entry->pk3);
+			while((hash_entry = (fsc_pk3_hash_map_entry_t *)STACKPTR(fsc_hashtable_next(&hti)))) {
+				const fsc_file_direct_t *pak = (const fsc_file_direct_t *)STACKPTR(hash_entry->pk3);
 				if(pak->source_dir_id == sourceid && !fs_file_disabled((fsc_file_t *)pak, 0)) {
 					char buffer[FS_FILE_BUFFER_SIZE];
 					fs_file_to_buffer((fsc_file_t *)pak, buffer, sizeof(buffer), qfalse, qtrue, qfalse, qfalse);

code/filesystem/fs_download.c
@@ -103,8 +103,8 @@ static qboolean entry_match_in_index(download_entry_t *entry, fsc_file_direct_t 
 	fsc_pk3_hash_map_entry_t *hashmap_entry;
 
 	fsc_hashtable_open(&fs.pk3_hash_lookup, entry->hash, &hti);
-	while((hashmap_entry = STACKPTR(fsc_hashtable_next(&hti)))) {
-		fsc_file_direct_t *pk3 = STACKPTR(hashmap_entry->pk3);
+	while((hashmap_entry = (fsc_pk3_hash_map_entry_t *)STACKPTR(fsc_hashtable_next(&hti)))) {
+		fsc_file_direct_t *pk3 = (fsc_file_direct_t *)STACKPTR(hashmap_entry->pk3);
 		if(!fsc_is_file_enabled((fsc_file_t *)pk3, &fs)) continue;
 		if(pk3->pk3_hash != entry->hash) continue;
 		if(fs_redownload_across_mods->integer &&
@@ -175,12 +175,12 @@ static qboolean fs_is_valid_download(download_entry_t *entry, unsigned int reche
 // Download List Creation
 /* ******************************************************************************** */
 
-static download_entry_t *create_download_entry(char *name, unsigned int hash) {
+static download_entry_t *create_download_entry(const char *name, unsigned int hash) {
 	// Returns new entry on success, null on error.
 	// Download entries should be freed by fs_free_download_entry.
 	download_entry_t *entry;
 	char temp_mod_dir[FSC_MAX_MODDIR];
-	char *temp_filename;
+	const char *temp_filename = 0;
 	char mod_dir[FSC_MAX_MODDIR];
 	char filename[MAX_DOWNLOAD_NAME + 1];
 
@@ -195,7 +195,7 @@ static download_entry_t *create_download_entry(char *name, unsigned int hash) {
 	if(!Q_stricmp(mod_dir, FS_GetCurrentGameDir())) Q_strncpyz(mod_dir, FS_GetCurrentGameDir(), sizeof(mod_dir));
 
 	// Set the download entry strings
-	entry = Z_Malloc(sizeof(*entry));
+	entry = (download_entry_t *)Z_Malloc(sizeof(*entry));
 	entry->local_name = CopyString(va("%s/%s%s.pk3", mod_dir, fs_saveto_dlfolder->integer ? "downloads/" : "", filename));
 	entry->remote_name = CopyString(va("%s.pk3", name));
 	entry->mod_dir = CopyString(mod_dir);
@@ -313,7 +313,7 @@ void fs_finalize_download(void) {
 			Com_Printf("WARNING: Downloaded pk3 %s has unexpected hash.\n", current_download->local_name); } }
 
 	if(FS_FileInPathExists(target_path)) {
-		char *new_name = va("%s/%s%s.%08x.pk3", current_download->mod_dir, fs_saveto_dlfolder->integer ? "downloads/" : "",
+		const char *new_name = va("%s/%s%s.%08x.pk3", current_download->mod_dir, fs_saveto_dlfolder->integer ? "downloads/" : "",
 				current_download->filename, actual_hash);
 		Com_Printf("WARNING: Downloaded pk3 %s conflicts with existing file. Using name %s instead.\n",
 				current_download->local_name, new_name);

code/filesystem/fs_fileio.c
@@ -115,9 +115,9 @@ static qboolean fs_generate_subpath(fsc_stream_t *stream, const char *path, int 
 
 	return qtrue; }
 
-int fs_generate_path(const char *path1, const char *path2, const char *path3,
+unsigned int fs_generate_path(const char *path1, const char *path2, const char *path3,
 		int path1_flags, int path2_flags, int path3_flags,
-		char *target, int target_size) {
+		char *target, unsigned int target_size) {
 	// Concatenates paths, adding '/' character as seperator, with sanitization
 	//    and directory creation based on flags
 	// Returns output length on success, 0 on error (overflow or sanitize fail)
@@ -140,15 +140,15 @@ int fs_generate_path(const char *path1, const char *path2, const char *path3,
 		return 0; }
 	return stream.position; }
 
-int fs_generate_path_sourcedir(int source_dir_id, const char *path1, const char *path2,
-		int path1_flags, int path2_flags, char *target, int target_size) {
+unsigned int fs_generate_path_sourcedir(int source_dir_id, const char *path1, const char *path2,
+		int path1_flags, int path2_flags, char *target, unsigned int target_size) {
 	// Generates path prefixed by source directory
 	if(!fs_sourcedirs[source_dir_id].active) return 0;
 	return fs_generate_path(fs_sourcedirs[source_dir_id].path_cvar->string, path1, path2, FS_NO_SANITIZE,
 				path1_flags, path2_flags, target, target_size); }
 
-int fs_generate_path_writedir(const char *path1, const char *path2, int path1_flags, int path2_flags,
-		char *target, int target_size) {
+unsigned int fs_generate_path_writedir(const char *path1, const char *path2, int path1_flags, int path2_flags,
+		char *target, unsigned int target_size) {
 	// Generates path prefixed by write directory
 	if(fs_read_only) return 0;
 	return fs_generate_path_sourcedir(0, path1, path2, path1_flags, path2_flags, target, target_size); }
@@ -226,7 +226,7 @@ static cache_entry_t *cache_lookup_table[CACHE_LOOKUP_TABLE_SIZE];
 
 static unsigned int cache_hash(const fsc_file_t *file) {
 	if(!file) return 0;
-	return fsc_string_hash(STACKPTR(file->qp_name_ptr), STACKPTR(file->qp_dir_ptr)); }
+	return fsc_string_hash((const char *)STACKPTR(file->qp_name_ptr), (const char *)STACKPTR(file->qp_dir_ptr)); }
 
 static void cache_lookup_table_register(cache_entry_t *entry) {
 	int position = entry->lookup_hash % CACHE_LOOKUP_TABLE_SIZE;
@@ -346,7 +346,7 @@ void fs_cache_initialize(void) {
 	if(cache_megs > 1024) cache_megs = 1024;
 
 	cache_size = cache_megs << 20;
-	base_entry = fsc_malloc(cache_size);
+	base_entry = (cache_entry_t *)fsc_malloc(cache_size);
 	head_entry = 0; }
 
 void fs_advance_cache_stage(void) {
@@ -496,7 +496,7 @@ char *fs_read_data(const fsc_file_t *file, const char *path, unsigned int *size_
 	if(cache_entry) {
 		++cache_entry->lock_count;
 		data = CACHE_ENTRY_DATA(cache_entry); }
-	else data = fsc_malloc(size + 1);
+	else data = (char *)fsc_malloc(size + 1);
 
 	// Extract data into buffer
 	if(fsc_file_handle) {
@@ -537,10 +537,10 @@ char *fs_read_shader(const fsc_shader_t *shader) {
 	char *shader_data;
 
 	if(size > 10000) return 0;
-	source_data = fs_read_data(STACKPTR(shader->source_file_ptr), 0, 0);
+	source_data = fs_read_data((const fsc_file_t *)STACKPTR(shader->source_file_ptr), 0, 0);
 	if(!source_data) return 0;
 
-	shader_data = Z_Malloc(size + 1);
+	shader_data = (char *)Z_Malloc(size + 1);
 	fsc_memcpy(shader_data, source_data + shader->start_position, size);
 	shader_data[size] = 0;
 	fs_free_data(source_data);
@@ -618,7 +618,7 @@ static fileHandle_t fs_allocate_handle(fs_handle_type_t type) {
 		default: Com_Error(ERR_DROP, "fs_allocate_handle with invalid type"); }
 
 	// Allocate
-	handles[index] = Z_Malloc(size);
+	handles[index] = (fs_handle_t *)Z_Malloc(size);
 	handles[index]->type = type;
 	handles[index]->owner = FS_HANDLEOWNER_SYSTEM;
 	return index + 1; }
@@ -1021,10 +1021,10 @@ static void fs_handle_set_owner(fileHandle_t handle, fs_handle_owner_t owner) {
 
 fs_handle_owner_t fs_handle_get_owner(fileHandle_t handle) {
 	fs_handle_t *handle_entry = fs_get_handle_entry(handle);
-	if(!handle_entry) return 0;
+	if(!handle_entry) return FS_HANDLEOWNER_SYSTEM;
 	return handle_entry->owner; }
 
-static char *identify_handle_type(fs_handle_type_t type) {
+static const char *identify_handle_type(fs_handle_type_t type) {
 	if(type == FS_HANDLE_CACHE_READ) return "cache read";
 	if(type == FS_HANDLE_DIRECT_READ) return "direct read";
 	if(type == FS_HANDLE_PK3_READ) return "pk3 read";
@@ -1032,7 +1032,7 @@ static char *identify_handle_type(fs_handle_type_t type) {
 	if(type == FS_HANDLE_PIPE) return "pipe";
 	return "unknown"; }
 
-static char *identify_handle_owner(fs_handle_owner_t owner) {
+static const char *identify_handle_owner(fs_handle_owner_t owner) {
 	if(owner == FS_HANDLEOWNER_SYSTEM) return "system";
 	if(owner == FS_HANDLEOWNER_CGAME) return "cgame";
 	if(owner == FS_HANDLEOWNER_UI) return "ui";
@@ -1086,7 +1086,7 @@ char *fs_read_journal_data(void) {
 		if(cache_entry) {
 			++cache_entry->lock_count;
 			data = CACHE_ENTRY_DATA(cache_entry); }
-		else data = fsc_malloc(length + 1); }
+		else data = (char *)fsc_malloc(length + 1); }
 
 	// Attempt to read data
 	r = FS_Read(data, length, com_journalDataFile);
@@ -1261,18 +1261,18 @@ void FS_FCloseFile(fileHandle_t f) {
 	fs_handle_close(f); }
 
 int FS_Read(void *buffer, int len, fileHandle_t f) {
-	return fs_handle_read(buffer, len, f); }
+	return fs_handle_read((char *)buffer, len, f); }
 
 int FS_Read2(void *buffer, int len, fileHandle_t f) {
 	// This seems pretty much identical to FS_Read in the original filesystem as well
 	return FS_Read(buffer, len, f); }
 
 int FS_Write(const void *buffer, int len, fileHandle_t h) {
-	fs_write_handle_write(h, buffer, len);
+	fs_write_handle_write(h, (const char *)buffer, len);
 	return len; }
 
 int FS_Seek(fileHandle_t f, long offset, int origin) {
-	return fs_handle_seek(f, offset, origin); }
+	return fs_handle_seek(f, offset, (fsOrigin_t)origin); }
 
 int FS_FTell(fileHandle_t f) {
 	return fs_handle_ftell(f); }
@@ -1309,7 +1309,7 @@ long FS_ReadFile(const char *qpath, void **buffer) {
 	return (long)len; }
 
 void FS_FreeFile(void *buffer) {
-	fs_free_data(buffer); }
+	fs_free_data((char *)buffer); }
 
 void FS_WriteFile( const char *qpath, const void *buffer, int size ) {
 	// Copy from original filesystem

commit 7467da61d369e4005467acac09f303422176117b

new filesystem: Fix cache corruption bug introduced in a165cddbc462dc2c78fc7b40d4051097112ebd3c

code/filesystem/fscore/fsc_misc.c
@@ -205,7 +205,7 @@ int fsc_stack_export(fsc_stack_t *stack, fsc_stream_t *stream) {
 	// Write each bucket (current position followed by data)
 	for(i=0; i<=stack->buckets_position; ++i) {
 		if(fsc_write_stream_data(stream, &stack->buckets[i]->position, 4)) return 1;
-		if(fsc_write_stream_data(stream, (char *)stack->buckets[i] + sizeof(stack->buckets[i]),
+		if(fsc_write_stream_data(stream, (char *)stack->buckets[i] + sizeof(fsc_stack_bucket_t),
 				stack->buckets[i]->position)) return 1; }
 
 	return 0; }
@@ -228,7 +228,7 @@ int fsc_stack_import(fsc_stack_t *stack, fsc_stream_t *stream) {
 		stack->buckets[i] = (fsc_stack_bucket_t *)fsc_calloc(STACK_BUCKET_SIZE);
 		if(fsc_read_stream_data(stream, &stack->buckets[i]->position, 4)) goto error;
 		if(stack->buckets[i]->position >= STACK_BUCKET_SIZE) goto error;
-		if(fsc_read_stream_data(stream, (char *)stack->buckets[i] + sizeof(stack->buckets[i]),
+		if(fsc_read_stream_data(stream, (char *)stack->buckets[i] + sizeof(fsc_stack_bucket_t),
 				stack->buckets[i]->position)) goto error; }
 	return 0;
 

code/filesystem/fscore/fscore.h
@@ -28,7 +28,7 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 // Definitions
 /* ******************************************************************************** */
 
-#define FSC_CACHE_VERSION 6
+#define FSC_CACHE_VERSION 7
 
 #define FSC_MAX_QPATH 256	// Buffer size including null terminator
 #define FSC_MAX_MODDIR 32	// Buffer size including null terminator