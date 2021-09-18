Chomenor/ioef-cmod

commit 0d221ccc16b05deb07a5479542991c36f07bfcbe

cmod: Move cvar_end_session call from CL_Disconnect to CL_ShutdownAll
This fixes some potential issues involving cvars being reset during HTTP downloads.

code/client/cl_main.c
@@ -1268,6 +1268,11 @@ CL_ShutdownAll
 */
 void CL_ShutdownAll(qboolean shutdownRef)
 {
+#ifdef CMOD_CVAR_HANDLING
+	if ( !com_sv_running->integer ) {
+		cvar_end_session();
+	}
+#endif
 	if(CL_VideoRecording())
 		CL_CloseAVI();
 
@@ -1449,11 +1454,6 @@ void CL_Disconnect( qboolean showMainMenu ) {
 	if ( !com_cl_running || !com_cl_running->integer ) {
 		return;
 	}
-
-#ifdef CMOD_CVAR_HANDLING
-	// Does this handle curl disconnects well?
-	if(clc.state > CA_DISCONNECTED) cvar_end_session();
-#endif
 
 	// shutting down the client so enter full screen ui mode
 	Cvar_Set("r_uiFullScreen", "1");

commit 1ee643abc0522ff51a12daa8e6439aa8f2ad29f3

cmod: Don't trigger end of session cvar reset unless cgame is running
Fixes bug when starting server from menu introduced in 0d221ccc16b05deb07a5479542991c36f07bfcbe

code/client/cl_main.c
@@ -1269,7 +1269,7 @@ CL_ShutdownAll
 void CL_ShutdownAll(qboolean shutdownRef)
 {
 #ifdef CMOD_CVAR_HANDLING
-	if ( !com_sv_running->integer ) {
+	if ( !com_sv_running->integer && cls.cgameStarted ) {
 		cvar_end_session();
 	}
 #endif