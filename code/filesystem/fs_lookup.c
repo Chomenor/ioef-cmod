dd654f601009e3b31ee19684f9281fa86a34a1ff
Noah Metzger <chomenor@gmail.com>
2021-12-28 19:23:19 -0600

    cmod: Update game module hashes

--- code/filesystem/fs_lookup.c
+++ code/filesystem/fs_lookup.c
@@ -923,6 +923,7 @@ static const int stock_qvms[] = {
 	-749739206,		// pakcmod-current-2021-09-25.pk3
 	-1518584883,	// pakcmod-current-2021-10-15.pk3
 	34943118,		// pakcmod-current-2021-11-11.pk3
+	1803491023,		// pakcmod-current-2021-12-03.pk3
 };
 #endif
 
