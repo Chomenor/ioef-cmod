7095006414605476c456df95d61216a66e02ab9e
Tim Angus <tim@ngus.net>
2025-07-14 15:29:22 +0100

    Update libjpeg

--- misc/update-libs.sh
+++ misc/update-libs.sh
@@ -12,18 +12,20 @@ prepare()
     shift
     local EXCLUDE_PATTERNS=("$@")
     local FILENAME=$(basename ${URL})
-    local BASENAME="${FILENAME%.tar.gz}"
-    local DIR="${CODE}/${BASENAME}"
 
     local EXCLUDE_ARGS= 
     for EXCLUDE_PATTERN in "${EXCLUDE_PATTERNS[@]}"; do
         EXCLUDE_ARGS+=" -not -regex ${EXCLUDE_PATTERN}"
     done
 
-    echo ${BASENAME}
-    curl -L "${URL}" | tar -xz -C "${CODE}"
+    echo ${FILENAME}
+
+    local EXTRACT_LOG=$(mktemp)
+    curl -sL "${URL}" | tar -xvz -C "${CODE}" | tee "${EXTRACT_LOG}"
+    local DIR=$(head -n1 "${EXTRACT_LOG}")
+
     (
-        cd ${DIR}
+        cd ${CODE}/${DIR}
         [ -f ./configure ] && ./configure
         find . -type f ${EXCLUDE_ARGS[@]} -delete
         find . -type d -empty -delete
@@ -35,3 +37,4 @@ prepare "https://downloads.xiph.org/releases/vorbis/libvorbis-1.3.7.tar.gz" "\./
 prepare "https://downloads.xiph.org/releases/opus/opus-1.5.2.tar.gz" "\./\(celt\|include\|silk\|src\)/.*\.[ch]"
 prepare "https://downloads.xiph.org/releases/opus/opusfile-0.12.tar.gz" "\./\(include\|src\)/.*\.[ch]"
 prepare "https://zlib.net/zlib-1.3.1.tar.gz" "./[^/]*\.[ch]"
+prepare "https://www.ijg.org/files/jpegsrc.v9f.tar.gz" "./[^/]*\.[ch]"
