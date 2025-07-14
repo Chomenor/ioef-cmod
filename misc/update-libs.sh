7af17542d23c94cd01ba7d2bf355caf489c9f12a
Tim Angus <tim@ngus.net>
2025-07-14 13:08:38 +0100

    Update zlib

--- misc/update-libs.sh
+++ misc/update-libs.sh
@@ -34,3 +34,4 @@ prepare "https://downloads.xiph.org/releases/ogg/libogg-1.3.6.tar.gz" "\./\(incl
 prepare "https://downloads.xiph.org/releases/vorbis/libvorbis-1.3.7.tar.gz" "\./\(include\|lib\)/.*\.[ch]"
 prepare "https://downloads.xiph.org/releases/opus/opus-1.5.2.tar.gz" "\./\(celt\|include\|silk\|src\)/.*\.[ch]"
 prepare "https://downloads.xiph.org/releases/opus/opusfile-0.12.tar.gz" "\./\(include\|src\)/.*\.[ch]"
+prepare "https://zlib.net/zlib-1.3.1.tar.gz" "./[^/]*\.[ch]"
