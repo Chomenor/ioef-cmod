dede9aa79ad5d4c1e4217370ae94a83b5a63c7b7
Tim Angus <tim@ngus.net>
2025-07-18 14:41:25 +0100

    Revert "define guards to prevent warnings" (fixes #737)
    This reverts commit 20634976e91b1e6380e34735bfc74a18505cc356.

--- code/qcommon/net_ip.c
+++ code/qcommon/net_ip.c
@@ -46,22 +46,10 @@ typedef int socklen_t;
 typedef unsigned short sa_family_t;
 #	endif
 
-#	ifndef EAGAIN
-#		define EAGAIN					WSAEWOULDBLOCK
-#	endif
-
-#	ifndef EADDRNOTAVAIL
-#		define EADDRNOTAVAIL	WSAEADDRNOTAVAIL
-#	endif
-
-#	ifndef EAFNOSUPPORT
-#		define EAFNOSUPPORT		WSAEAFNOSUPPORT
-#	endif
-
-#	ifndef ECONNRESET
-#		define ECONNRESET			WSAECONNRESET
-#	endif
-
+#	define EAGAIN					WSAEWOULDBLOCK
+#	define EADDRNOTAVAIL	WSAEADDRNOTAVAIL
+#	define EAFNOSUPPORT		WSAEAFNOSUPPORT
+#	define ECONNRESET			WSAECONNRESET
 typedef u_long	ioctlarg_t;
 #	define socketError		WSAGetLastError( )
 
