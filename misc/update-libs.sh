279f4e52393feff38b56ad9f55165dda9b52cfe8
Tim Angus <tim@ngus.net>
2025-07-20 22:13:31 +0100

    Be slightly more targetted in cleaning up jpeg source

--- misc/update-libs.sh
+++ misc/update-libs.sh
@@ -9,14 +9,9 @@ CODE="${ROOT}/code"
 prepare()
 {
     local URL="$1"
-    shift
-    local EXCLUDE_PATTERNS=("$@")
+    local INCLUDE_PATTERN="$2"
+    local EXCLUDE_PATTERN="$3"
     local FILENAME=$(basename ${URL})
-
-    local EXCLUDE_ARGS= 
-    for EXCLUDE_PATTERN in "${EXCLUDE_PATTERNS[@]}"; do
-        EXCLUDE_ARGS+=" -not -regex ${EXCLUDE_PATTERN}"
-    done
 
     echo ${FILENAME}
 
@@ -27,7 +22,8 @@ prepare()
     (
         cd ${CODE}/${DIR}
         [ -f ./configure ] && ./configure
-        find . -type f ${EXCLUDE_ARGS[@]} -delete
+        find . -type f -not -regex ${INCLUDE_PATTERN} -delete
+        find . -type f -regex ${EXCLUDE_PATTERN} -delete
         find . -type d -empty -delete
     )
 }
@@ -37,4 +33,4 @@ prepare "https://downloads.xiph.org/releases/vorbis/libvorbis-1.3.7.tar.gz" "\./
 prepare "https://downloads.xiph.org/releases/opus/opus-1.5.2.tar.gz" "\./\(celt\|include\|silk\|src\)/.*\.[ch]"
 prepare "https://downloads.xiph.org/releases/opus/opusfile-0.12.tar.gz" "\./\(include\|src\)/.*\.[ch]"
 prepare "https://zlib.net/zlib-1.3.1.tar.gz" "./[^/]*\.[ch]"
-prepare "https://www.ijg.org/files/jpegsrc.v9f.tar.gz" "./[^/]*\.[ch]"
+prepare "https://www.ijg.org/files/jpegsrc.v9f.tar.gz" "./\(j.*\.c\|.*\.h\)" "./jmem\(ansi\|dos\|mac\|name\)\.c"
