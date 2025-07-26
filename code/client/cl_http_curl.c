2e753008d58c8e66ca8a1e6f45c26db6586356b1
Martin Gerhardy <martin.gerhardy@gmail.com>
2025-07-26 21:08:40 +0200

    CLIENT: restore older curl version support

--- code/client/cl_http_curl.c
+++ code/client/cl_http_curl.c
@@ -295,7 +295,11 @@ void CL_HTTP_BeginDownload( const char *remoteURL )
 	qcurl_easy_setopt_warn(downloadCURL, CURLOPT_FAILONERROR, 1);
 	qcurl_easy_setopt_warn(downloadCURL, CURLOPT_FOLLOWLOCATION, 1);
 	qcurl_easy_setopt_warn(downloadCURL, CURLOPT_MAXREDIRS, 5);
+#if CURL_AT_LEAST_VERSION(7,85,0)
 	qcurl_easy_setopt_warn(downloadCURL, CURLOPT_PROTOCOLS_STR, "http,https");
+#else
+	qcurl_easy_setopt_warn(downloadCURL, CURLOPT_PROTOCOLS, CURLPROTO_HTTP | CURLPROTO_HTTPS);
+#endif
 	qcurl_easy_setopt_warn(downloadCURL, CURLOPT_BUFFERSIZE, CURL_MAX_READ_SIZE);
 	downloadCURLM = qcurl_multi_init();
 	if(!downloadCURLM) {
